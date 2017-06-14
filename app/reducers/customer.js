8332adf0e568a03e6cd9f7b125077fca0b1fdbb6
DMITRY STARODUBTSEV <dmitry.starodubcev@gmail.com>
2017-06-14 23:11:32 +0300

    Save token and username in localStorage

--- app/reducers/customer.js
+++ app/reducers/customer.js
@@ -1,17 +1,28 @@
 import constants from '../constants';
 
+const initialState = {
+    token: window.localStorage.getItem('token'),
+    username: window.localStorage.getItem('username'),
+    loading: false,
+};
 
-exports.customerReducer = function (state={token: null, username: null, loading: false}, action) {
+exports.customerReducer = function (state=initialState, action) {
     switch (action.type) {
         case constants.SET_TOKEN:
+            const token = action.payload;
+            window.localStorage.setItem('token', token);
             return Object.assign(
-                {}, state, {token: action.payload, loading: false}
+                {}, state, {token: token, loading: false}
             );
         case constants.SET_USERNAME:
+            const username = action.payload;
+            window.localStorage.setItem('username', username);
             return Object.assign(
-                {}, state, {username: action.payload, loading: false}
+                {}, state, {username: username, loading: false}
             );
         case constants.LOGOUT:
+            window.localStorage.removeItem('token');
+            window.localStorage.removeItem('username');
             return Object.assign(
                 {}, state, {username: null, token: null, loading: false}
             );
