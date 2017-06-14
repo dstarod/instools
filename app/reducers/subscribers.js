629fb1c0f482c426fcb624c6683de8cbdc10606f
DMITRY STARODUBTSEV <dmitry.starodubcev@gmail.com>
2017-06-14 23:34:34 +0300

    Add a couple of cases to subscribers reducer

--- app/reducers/subscribers.js
+++ app/reducers/subscribers.js
@@ -4,6 +4,34 @@ import {convertRawUser} from './helpers';
 
 exports.subscriberListReducer = function (state={users: [], loading: false, loaded: false}, action) {
     switch (action.type) {
+        case constants.USER_CHANGES_IN_PROGRESS:
+            let new_users = state.users.map(
+                function(user){
+                    if(user.id === action.payload){
+                        user.followed = constants.USER_CHANGES_IN_PROGRESS;
+                    }
+                    return user;
+                }
+            );
+            return Object.assign(
+                {}, state, {
+                    users: new_users,
+                    loading: false,
+                    loaded: true,
+                }
+            );
+        case constants.SUBSCRIBER_REMOVE:
+            return Object.assign(
+                {}, state, {
+                    users: state.users.filter(
+                        function(user){
+                            return user.id !== action.payload
+                        }
+                    ),
+                    loading: false,
+                    loaded: true,
+                }
+            );
         case constants.SUBSCRIBERS_DROP_CACHE:
             return Object.assign(
                 {}, state, {
